e0289edabc2423ee53f89c5205a5da8eafeb823a
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-08 14:51:01 -0400

    2-fans.sql

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- SQL SCRIPT THAT RANKS COUNTRY ORIGINS OF BANDS
+-- ORDERED BY THE NUMBER OF (NON-UNIQUE) FANS
+SELECT
+    origin,
+    nb_fans
+WHERE
+    nb_fans = SUM(fans)
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
